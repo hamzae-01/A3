a6a0695f16adb28d37e3195ad6599a34e54a00dd
hamza <hamza@10.20.79.23>
2019-09-23 00:06:48 -0400

    novice 9

--- src/a3/CompletedRideImpl.java
+++ src/a3/CompletedRideImpl.java
@@ -1,66 +1,79 @@
 package a3;
 
 public class CompletedRideImpl implements CompletedRide {
-	
-	RideRequest _request;
-	Driver _driver;
+
+	private RideRequest Request;
+	private Driver Driver;
+	private int WaitTime;
+	private int TotalTime;
+	private Position driverposition;
+	private Position clientposition;
+	private double Cost;
+	private double Price;
+	private double Profit;
 
 	public CompletedRideImpl(RideRequest request, Driver driver) {
-		this._request = request;
-		this._driver = driver;
+
+		this.Request = request;
+		this.Driver = driver;
+		this.WaitTime = WaitTime;
+		this.driverposition = driver.getVehicle().getPosition();
+		this.clientposition = request.getClientPosition();
+
+		this.TotalTime = TotalTime;
+		this.Cost = Cost;
+		this.Price = Price;
+		this.Profit = Profit;
+
 	}
-	
-	
-	
+
+	@Override
 	public RideRequest getRequest() {
-		
-		return _request;
+// TODO Auto-generated method stub
+		return Request;
 	}
 
-	
+	@Override
 	public Driver getDriver() {
-		
-		return _driver;
+// TODO Auto-generated method stub
+		return Driver;
 	}
 
-	
 	public int getWaitTime() {
-		
-		return _request.getClientPosition().getManhattanDistanceTo(_driver.getVehicle().getPosition());
+// TODO Auto-generated method stub
+		return clientposition.getManhattanDistanceTo(driverposition);
 	}
 
-	
 	public int getTotalTime() {
-		
-		return (getWaitTime() + _request.getRideTime());
+// TODO Auto-generated method stub
+		return (Request.getRideTime() + getWaitTime());
 	}
-
 
 	public double getCost() {
-		return (((double) (.50 * getRequest().getRideTime())) + ((double) (.10 *getWaitTime())));
+// TODO Auto-generated method stub
+
+		return (((double) (.50 * getRequest().getRideTime())) + ((double) (.10 * getWaitTime())));
+
 	}
 
-	
 	public double getPrice() {
-		// TODO Auto-generated method stub
+// TODO Auto-generated method stub
 		double wait = getWaitTime();
 		double r = getRequest().getRideTime();
 
-
 		if (getWaitTime() < 25) {
-		return r * 2.5;
+			return r * 2.5;
 		} else if (getWaitTime() >= 25 && getWaitTime() <= 49) {
-		return r * 2;
+			return r * 2;
 		} else if (getWaitTime() >= 50 && getWaitTime() <= 99) {
-		return r ;
+			return r;
 		} else if (getWaitTime() >= 100) {
-		return r * .5;
+			return r * .5;
 		} else {
-		return 0;
+			return 0;
 		}
-		}
+	}
 
-	
 	public double getProfit() {
 
 		return (getPrice() - getCost());
